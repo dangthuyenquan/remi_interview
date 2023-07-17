40d6d9b831f9b89cab8d898283fdce45c0faf4ab
dangthuyenquan <dangthuyenquan@gmail.com>
2023-07-17 14:28:58 +0700

    [frontend] video service

---
+++ src/frontend/src/services/videoService.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const videoService = {
+    getAll: async function getAll(pagination: { pageSize: string, currentPage: string }): Promise<IResponseVideoAll | null> {
+        const result = await axios.get('/video?' + (new URLSearchParams(pagination).toString()));
+
+        if (result.data) {
+            return result.data as IResponseVideoAll;
+        }
+
+        return null;
+    },
+
+    create: async function (videoYoutubeId: string) {
+        return await axios.post('/video', { videoYoutubeId });
+    }
+
+}
+
+export default videoService;
+
+export interface IVideo {
+    _id: string,
+    videoYoutubeId: string,
+    title: string,
+    description: string,
+}
+
+export interface IResponseVideoAll {
+    currentPage: number,
+    pageSize: number,
+    totalPages: number,
+    data: Array<IVideo>
+}
